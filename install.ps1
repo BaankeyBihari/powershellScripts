cb690d48839950227ba5086afdba4b9801ef740a
Sudhanshu <sudhanshu@baruntar.com>
2022-05-31 20:52:10 +0530

    Test Commit 003

--- install.ps1
+++ install.ps1
@@ -1,11 +1,44 @@
 $hostname = hostname
+
+$projectDir = "$env:localappdata/powershellScripts/"
+try {
+    New-Item -Type Directory -Path $projectDir -ErrorAction SilentlyContinue
+}
+catch {
+    Write-Output "Already there"
+}
+
 $resourcePath = "$env:localappdata/powershellScripts/resource.json"
-echo $resourcePath
 
 if($hostname.toLower().contains("dungeon")) {
     Write-Output "Match"
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/work.json
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/dungeon.json -OutFile $resourcePath
 } else {
     Write-Output "No Match"
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/work.json
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/work.json -OutFile $resourcePath
+}
+
+$config = Get-Content $resourcePath -Raw | ConvertFrom-Json
+
+function scoopManager() {
+    try {
+        Get-Command scoop -ErrorAction Stop
+    }
+    catch {
+        Write-Output "Installing scoop"
+        Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+        irm get.scoop.sh | iex
+    }
+}
+
+function chocoManager() {
+    Write-Output $config
+}
+
+function wingetManager() {
+    Write-Output $config
+}
+
+foreach ($installer in $config.install) {
+    Write-Output $installer
 }
