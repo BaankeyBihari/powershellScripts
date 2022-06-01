b8a7158856f798b635e975d46ae8ab073e0bf279
Sudhanshu <sudhanshu@baruntar.com>
2022-06-01 07:12:22 +0530

    Test Commit 005

--- install.ps1
+++ install.ps1
@@ -9,17 +9,19 @@ catch {
 }
 
 $resourcePath = "$env:localappdata/powershellScripts/resource.json"
+rm $resourcePath
 
 if($hostname.toLower().contains("dungeon")) {
     Write-Output "Match"
     Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/dungeon.json -OutFile $resourcePath
 } else {
     Write-Output "No Match"
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/work.json -OutFile $resourcePath
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/BaankeyBihari/powershellScripts/main/others.json -OutFile $resourcePath
 }
 
 $config = Get-Content $resourcePath -Raw | ConvertFrom-Json
 
+$scoopConfig
 function scoopManager() {
     try {
         Get-Command scoop -ErrorAction Stop
@@ -29,30 +31,57 @@ function scoopManager() {
         Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
         irm get.scoop.sh | iex
     }
+    foreach ($bucket in $scoopConfig.buckets) {
+        if(Get-Member -inputobject $bucket -name "link" -Membertype Properties) {
+            scoop bucket add $bucket.name $bucket.link
+        } else {
+            scoop bucket add $bucket.name
+        }
+    }
+    foreach ($item in $scoopConfig.items) {
+        scoop install $item
+    }
 }
 
+$chocoConfig
 function chocoManager() {
-    Write-Output $config
+    try {
+        Get-Command choco -ErrorAction Stop
+    }
+    catch {
+        Write-Output "Installing choco"
+        sudo iex "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+    }
+    foreach ($item in $chocoConfig.items) {
+        sudo iex "choco install $item"
+    }
 }
 
+$wingetConfig
 function wingetManager() {
-    Write-Output $config
+    foreach ($item in $chocoConfig.items) {
+        winget install $item
+    }
 }
 
 foreach ($installer in $config.install) {
     switch($installer.source) {
         "scoop" {
             Write-Output "Found Scoop"
-            Write-Output $installer.items
-            Write-Output $installer.buckets
+            $scoopConfig = $installer
+            scoopManager
             break
         }
         "choco" {
             Write-Output "Found Chocolatey"
+            $chocoConfig = $installer
+            chocoManager
             break
         }
         "winget" {
             Write-Output "Found Winget"
+            $wingetConfig = $installer
+            wingetManager
             break
         }
     }
